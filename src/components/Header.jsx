bef16107e372da384e4b2af190ad4309e5b2186a
nadyaiva <79001245+nadyaiva@users.noreply.github.com>
2022-08-24 19:57:34 +0200

    feat: add log out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import logoMesto from "../images/logo.svg";
+import { Link, useHistory } from "react-router-dom";
 
-function Header({isLoggedIn, email}) {
-  const emailClassName = `header__userEmail ${isLoggedIn ? 'header__userEmail_visible' : ''}`
-  const loginText = isLoggedIn ? 'Выйти' : 'Регистрация'
+function Header({ isLoggedIn, email }) {
+  const emailClassName = `header__username ${
+    isLoggedIn ? "header__username_visible" : ""
+  }`;
+  const loginText = isLoggedIn ? "Выйти" : "Регистрация";
+  const loginClassName = `header__button ${
+    isLoggedIn ? "header__button_notfocus" : ""
+  }`;
+  const history = useHistory();
+
+  function signOut() {
+    localStorage.removeItem("token");
+    history.push("/login");
+  }
+
   return (
     <header className="header">
       <img className="header__logo" src={logoMesto} alt="Логотип Mesto" />
-      <div className="header__login-info">
-        <p className={emailClassName}>{email}</p>
-      <a className="header__link header__link_" href="#">`${isLoggedIn} ? Выйти : Регистрация`</a>
-      </div>
-      
+      <ul className="header__list-info">
+        <li className={emailClassName}>
+          <p className="header__email">{email}</p>
+        </li>
+        <li>
+          <button onClick={signOut} className={loginClassName} href="#">{loginText}</button>
+        </li>
+      </ul>
     </header>
   );
 }
