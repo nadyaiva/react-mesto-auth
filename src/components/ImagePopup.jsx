f05fb9a257c79e8f20f68fb9ef744f6875fc0a9e
nadyaiva <79001245+nadyaiva@users.noreply.github.com>
2022-08-28 15:47:23 +0200

    refactor: edit fullscreen image popup

--- src/components/ImagePopup.jsx
+++ src/components/ImagePopup.jsx
@@ -2,34 +2,12 @@ import React from "react";
 import Popup from "./Popup";
 
 function ImagePopup({card, onClose }) {
-  // const classNamePopupImage = `popup popup-fullscreen ${
-  //   !!card ? "popup_opened" : ""
-  // }`;
   const cardLink = card ? card.link : "";
   const cardName = card ? card.name : "";
 
   return (
-    // <div className={classNamePopupImage}>
-    //   <div>
-    //   <figure className="popup-fullscreen__figure">
-    //     <img
-    //       className="popup-fullscreen__image"
-    //       src={cardLink}
-    //       alt={cardName}
-    //     />
-    //     <figcaption className="popup-fullscreen__caption">
-    //       {cardName}
-    //     </figcaption>
-    //     <button
-    //       className="popup__close popup__close-button popup__close-button_place_fullscreen"
-    //       type="button"
-    //       onClick={onClose}
-    //     ></button>
-    //   </figure>
-    //   </div>
-    // </div>);
     <Popup isOpen={card} name={'image'} onClose={onClose}>
-
+        <figure className="popup-fullscreen__figure">
         <img
           className="popup-fullscreen__image"
           src={cardLink}
@@ -38,6 +16,7 @@ function ImagePopup({card, onClose }) {
         <figcaption className="popup-fullscreen__caption">
           {cardName}
         </figcaption>
+        </figure>
     </Popup>
   );
 }
